6161ecd8c30082dffddb019d24164aae316ff805
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2025-10-31 13:01:30 -0400

    how about this then

--- src/hooks/LoadingLayer.cpp
+++ src/hooks/LoadingLayer.cpp
@@ -6,23 +6,31 @@ class $modify(SoggifyHookLL, LoadingLayer) {
     bool init(bool refresh) {
         if (!LoadingLayer::init(refresh)) return false;
         if (Mod::get()->getSettingValue<bool>("ery-mode")) return true;
-        if (!refresh) {
-            if (Mod::get()->hasSavedValue("quests")) {
-                Mod::get()->getSaveContainer().erase("quests");
-            }
-        }
-        if (!Mod::get()->hasSavedValue("i_1")) {
-            Mod::get()->setSavedValue("i_1", true);
-        }
         if (Mod::get()->getSavedValue<bool>("i_1")) {
-            if (!Loader::get()->isPatchless()) {
-                Loader::get()->queueInMainThread([this]() {
-                    if (auto node = static_cast<CCSprite*>(getChildByID("gd-logo"))) {
-                        sogutils::sog(node);
-                    }
-                });
-            }
+            Loader::get()->queueInMainThread([this]() {
+                if (auto node = static_cast<CCSprite*>(getChildByID("gd-logo"))) {
+                    sogutils::sog(node);
+                }
+            });
         }
         return true;
     }
 };
+
+$execute {
+    if (Loader::get()->isPatchless()) {
+        sogutils::toggleLayerHook("LoadingLayer::init", false);
+    }
+}
+
+$on_mod(Loaded) {
+    if (Loader::get()->isPatchless()) {
+        sogutils::toggleLayerHook("LoadingLayer::init", false);
+    }
+    if (Mod::get()->hasSavedValue("quests")) {
+        Mod::get()->getSaveContainer().erase("quests");
+    }
+    if (!Mod::get()->hasSavedValue("i_1")) {
+        Mod::get()->setSavedValue("i_1", true);
+    }
+}
